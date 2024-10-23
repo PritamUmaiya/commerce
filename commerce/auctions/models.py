211283cb0da364a08296a6d1cb874564feee805c
pritamjaiswal.me <pritamjaiswal.me@gmail.com>
2024-10-23 03:06:33 +0000

    created migration

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -5,3 +5,29 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+
+class Auction(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=200)
+    starting_bid = models.IntegerField()
+    image = models.CharField(max_length=200, blank=True)
+    category = models.CharField(max_length=64, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctions")
+    active = models.BooleanField(default=True)
+
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+
+
+class Bids(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    bid = models.IntegerField()
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
