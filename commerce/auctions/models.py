4356a4c8583e2fc483952d6478f38407dcda6435
pritamumaiya <pritamumaiya@gmail.com>
2024-11-12 04:51:04 +0000

    adding models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,31 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    starting_bid = models.IntegerField()
+    image_url = models.CharField(max_length=200, null=True, blank=True)
+    category = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    bid_amount = models.IntegerField()
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+
+
+
+
+    
+
+    
+
